d02b6c072ea128451f33529bd6c76f7f47e47475
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2024-06-28 12:14:31 +0100

    Initial scaffold of UTR-related pages (#7)

---
+++ app/forms/SoleTraderNameFormProvider.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import javax.inject.Inject
+
+import forms.mappings.Mappings
+import play.api.data.Form
+import play.api.data.Forms._
+import models.SoleTraderName
+
+class SoleTraderNameFormProvider @Inject() extends Mappings {
+
+   def apply(): Form[SoleTraderName] = Form(
+     mapping(
+      "firstName" -> text("soleTraderName.error.firstName.required")
+        .verifying(maxLength(100, "soleTraderName.error.firstName.length")),
+      "lastName" -> text("soleTraderName.error.lastName.required")
+        .verifying(maxLength(100, "soleTraderName.error.lastName.length"))
+    )(SoleTraderName.apply)(x => Some((x.firstName, x.lastName)))
+   )
+ }
--- test-utils/generators/ModelGenerators.scala
+++ test-utils/generators/ModelGenerators.scala
@@ -22,6 +22,14 @@ import org.scalacheck.{Arbitrary, Gen}
 
 trait ModelGenerators {
 
+  implicit lazy val arbitrarySoleTraderName: Arbitrary[SoleTraderName] =
+    Arbitrary {
+      for {
+        firstName <- arbitrary[String]
+        lastName <- arbitrary[String]
+      } yield SoleTraderName(firstName, lastName)
+    }
+
   implicit lazy val arbitraryBusinessType: Arbitrary[BusinessType] =
     Arbitrary {
       Gen.oneOf(BusinessType.values.toSeq)
