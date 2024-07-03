9c72ea6ac3143add1594b97dec55c2dbf4a9c469
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2024-07-03 11:29:41 +0100

    Scaffold address pages

---
+++ test/pages/InternationalAddressPageSpec.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pages
+
+import controllers.routes
+import models.{CheckMode, NormalMode, UserAnswers}
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+
+class InternationalAddressPageSpec extends AnyFreeSpec with Matchers {
+
+  ".nextPage" - {
+
+    val emptyAnswers = UserAnswers("id")
+
+    "in Normal Mode" - {
+
+      "must go to Index" in {
+
+        InternationalAddressPage.nextPage(NormalMode, emptyAnswers) mustEqual routes.IndexController.onPageLoad()
+      }
+    }
+
+    "in Check Mode" - {
+
+      "must go to Check Answers" in {
+
+        InternationalAddressPage.nextPage(CheckMode, emptyAnswers) mustEqual routes.CheckYourAnswersController.onPageLoad()
+      }
+    }
+  }
+}
