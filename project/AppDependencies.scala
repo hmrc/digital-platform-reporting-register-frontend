dae6d6ea1942ba79f0d577a6f29f5e78115cd858
ZaraFarzan <177644563+ZaraFarzan@users.noreply.github.com>
2026-06-26 09:19:13 +0100

    DLS-13134:[DPRS] Update Title to match H1 (Register)
    DLS-13134:[DPRS] Update Title to match H1 (Register)
    
    DLS-13134:[DPRS] Update Title to match H1 (Register)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,11 +7,11 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30" % "13.7.0",
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30" % "13.9.0",
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "org.typelevel"                 %% "cats-core"                  % "2.13.0",
-    "com.googlecode.libphonenumber"  % "libphonenumber"             % "9.0.27",
+    "com.googlecode.libphonenumber"  % "libphonenumber"             % "9.0.33",
     "uk.gov.hmrc"                   %% "crypto-json-play-30"        % "8.4.0",
   )
 
